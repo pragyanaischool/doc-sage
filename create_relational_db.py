f920ad6b01ea893b840a014a9fbb67c3cf552f4b
NGONIDZASHE NZENZE <ngonidzashedelight@gmail.com>
2024-11-04 16:25:01 +0200

    updated on db creation

--- create_relational_db.py
+++ create_relational_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # Connect to SQLite database (or create it if it doesn't exist)
-conn = sqlite3.connect("study_sage.sqlite")
+conn = sqlite3.connect("doc_sage.sqlite")
 cursor = conn.cursor()
 
 # Create 'chat' table
